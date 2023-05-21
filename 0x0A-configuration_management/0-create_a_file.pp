cb9fc2c106981c1def572128719aba6661f63cb8
Tumelo Joel Mtsweni <TumeloJ.Mtsweni@gmail.com>
2023-05-20 20:42:47 -0400

    Creating files using puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,8 @@
+# Creating a file using Puppet
 file { '/tmp/school':
   ensure  => file,
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',}
+  content => 'I love Puppet',
+}
