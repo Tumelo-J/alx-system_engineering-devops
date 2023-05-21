b85dd98cf2cbdb3d0a170906bb5fccc513f34c6e
Tumelo Joel Mtsweni <TumeloJ.Mtsweni@gmail.com>
2023-05-20 21:56:00 -0400

    Pkill

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
-# Pkill with Puppet
-exec { 'killmenow':
-  command     => 'pkill killmenow',
-  path        => '/bin:/usr/bin:/usr/local/bin',
-  refreshonly => true,
+# kill process killmenow
+
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
